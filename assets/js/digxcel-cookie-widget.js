a2d250251f8be087cc87567e70d97b1c504ec3aa
Ian Luddy <ian.luddy@softwaredesign.ie>
2018-07-04 17:13:34 +0100

    Cookie widget updates

--- assets/js/digxcel-cookie-widget.js
+++ assets/js/digxcel-cookie-widget.js
@@ -1,29 +1,29 @@
 
-const iframeId = "digxcel-modal";
-const bannerId = "digxcel-banner";
-const overlayId = "digxcel-overlay";
-const bannerDismissedCookie = "digxcel-banner-dismissed";
+const dxIframeId = "digxcel-modal";
+const dxBannerId = "digxcel-banner";
+const dxOverlayId = "digxcel-overlay";
+const dxBannerDismissedCookie = "digxcel-banner-dismissed";
 let dpsId = null;
-let modalReady = false;
+let dxModalReady = false;
 
 window.onload = function() {
   const iframeSource = document.getElementById("digxcelConfig").getAttribute("url");
   dpsId = iframeSource.split("?dpsId=")[1];
 
-  if(getCookie(bannerDismissedCookie) === "true")
-    return null;
-
   var iframe = document.createElement('iframe');
-  iframe.id = iframeId;
+  iframe.id = dxIframeId;
   iframe.src = iframeSource;
   iframe.style = "z-index: 1001; position: fixed; top: -100%; opacity: 0; max-height: 550px;";
   document.body.appendChild(iframe);
 
   var overlay = document.createElement('div');
-  overlay.id = overlayId;
+  overlay.id = dxOverlayId;
   overlay.style = "z-index: 1000; display: none; position: fixed; top: 0; right: 0; bottom: 0; left: 0; background-color: #000; opacity: 0.3;";
-  overlay.onclick = toggleIframe;
+  overlay.onclick = dxToggleIframe;
   document.body.appendChild(overlay);
+
+  if(dxGetCookie(dxBannerDismissedCookie) === "true")
+    return null;
 
   var banner = document.createElement('div');
   banner.innerHTML = `
@@ -31,71 +31,73 @@ window.onload = function() {
     <div style="position: fixed; bottom: 16px; right: 50px; color: rgb(221, 221, 221);">
       <span><h4 style="color: #ffffff; margin-bottom: 13px; margin-right: 9px;">This site uses Cookies to improve your experience</h4></span>
       <span style="float: right;">
-        <a style="margin-right: 10px;" href="#" onclick="toggleIframe()">Cookie Settings</a>
-        <a style="margin-right: 10px;" href="#" onclick="toggleBanner()">Dismiss</a>
+        <a style="margin-right: 10px;" href="#" onclick="dxToggleIframe()">Cookie Settings</a>
+        <a style="margin-right: 10px;" href="#" onclick="dxToggleBanner()">Dismiss</a>
       </span>
     </div>
   `;
   banner.style = "display: block;";
-  banner.id = bannerId;
+  banner.id = dxBannerId;
   document.body.appendChild(banner);
 }
 
-function toggleIframe() {
-  if( !modalReady )
+function dxToggleIframe() {
+  if( !dxModalReady )
     return null;
-  if( document.getElementById(iframeId).style.opacity == '1' ) {
-    document.getElementById(iframeId).style.opacity = '0';
-    document.getElementById(iframeId).style.top = '-100%';
+  if( document.getElementById(dxIframeId).style.opacity == '1' ) {
+    document.getElementById(dxIframeId).style.opacity = '0';
+    document.getElementById(dxIframeId).style.top = '-100%';
   } else {
-    document.getElementById(iframeId).style.opacity = '1';
-    document.getElementById(iframeId).style.top = '20%';
+    document.getElementById(dxIframeId).style.opacity = '1';
+    document.getElementById(dxIframeId).style.top = '20%';
   }
-  toggleById(overlayId);
+  dxToggleById(dxOverlayId);
 }
 
-function toggleBanner() {
-  toggleById(bannerId);
-  storeCookie(bannerDismissedCookie, "true", "Sat, 01-Jan-2050 00:00:00 GMT", "." + window.location.hostname);
+function dxToggleBanner() {
+  dxToggleById(dxBannerId);
+  dxStoreCookie(dxBannerDismissedCookie, "true", "Sat, 01-Jan-2050 00:00:00 GMT", "." + window.location.hostname, '/');
 }
 
-function resizeIframe(height, width) {
-  modalReady = true;
-  let iframe = document.getElementById(iframeId);
+function dxResizeIframe(height, width) {
+  dxModalReady = true;
+  let iframe = document.getElementById(dxIframeId);
   iframe.style.width = width + 'px';
   iframe.style.height = height + 'px';
-  centreIframe();
+  dxCentreIframe();
 }
 
-function centreIframe() {
-  let iframe = document.getElementById(iframeId);
-  document.getElementById(iframeId).style.left = ( window.innerWidth - parseInt(iframe.style.width) ) / 2  + 'px';
+function dxCentreIframe() {
+  let iframe = document.getElementById(dxIframeId);
+  if(iframe)
+    document.getElementById(dxIframeId).style.left = ( window.innerWidth - parseInt(iframe.style.width) ) / 2  + 'px';
 }
 
-function toggleById(elementId) {
-  let elementDisplay = document.getElementById(elementId).style.display;
-  document.getElementById(elementId).style.display = elementDisplay == 'block' ? 'none' : 'block';
+function dxToggleById(elementId) {
+  let element = document.getElementById(elementId);
+  if(element != null)
+    document.getElementById(elementId).style.display = element.style.display == 'block' ? 'none' : 'block';
 }
 
-function blockCookie(cookie) {
-  expireCookie(cookie.name, "expired", "Sat, 01-Jan-2000 00:00:00 GMT", cookie.domain, cookie.path);
-  expireCookie(cookie.name, "expired", "Sat, 01-Jan-2000 00:00:00 GMT", cookie.domain, '.' + cookie.path);
-  expireCookie(cookie.name, "expired", "Sat, 01-Jan-2000 00:00:00 GMT", cookie.domain, '/' + window.location.pathname.split('/').join(''));
+function dxBlockCookie(cookie) {
+  dxExpireCookie(cookie.name, "expired", "Sat, 01-Jan-2000 00:00:00 GMT", cookie.domain, cookie.path);
+  dxExpireCookie(cookie.name, "expired", "Sat, 01-Jan-2000 00:00:00 GMT", cookie.domain, '.' + cookie.path);
+  dxExpireCookie(cookie.name, "expired", "Sat, 01-Jan-2000 00:00:00 GMT", cookie.domain, '/' + window.location.pathname.split('/').join(''));
 }
 
-function getCookie(cookieName) {
+function dxGetCookie(cookieName) {
   var value = "; " + document.cookie;
   var parts = value.split("; " + cookieName + "=");
   if (parts.length == 2) return parts.pop().split(";").shift();
 }
 
-function expireCookie(id, value, expires, domain, path) {
+function dxExpireCookie(id, value, expires, domain, path) {
   document.cookie = `${id}=${value}; expires=${expires}; domain=${domain}; path=${path}`;
   document.cookie = `${id}=${value}; expires=${expires}; domain=${domain};`;
   document.cookie = `${id}=${value}; expires=${expires};`;
 }
 
-function storeCookie(id, value, expires, domain, path) {
+function dxStoreCookie(id, value, expires, domain, path) {
   if( domain && path ){
     document.cookie = `${id}=${value}; expires=${expires}; domain=${domain}; path=${path}`;
   } else if ( domain ) {
@@ -105,29 +107,29 @@ function storeCookie(id, value, expires, domain, path) {
   }
 }
 
-function syncConsents(consentCookieId, consents) {
+function dxSyncConsents(consentCookieId, consents) {
   if( consentCookieId.indexOf(dpsId) != -1 ){
-    storeCookie("digxcel-consents", consents, "Sat, 01-Jan-2050 00:00:00 GMT", window.location.hostname, "/");
+    dxStoreCookie("digxcel-consents", consents, "Sat, 01-Jan-2050 00:00:00 GMT", window.location.hostname, "/");
   }
 }
 
 window.onresize = function(event) {
-  centreIframe();
+  dxCentreIframe();
 };
 
 window.addEventListener("message", function(event) {
   switch (event.data.action) {
     case "block":
-      blockCookie(event.data.data);
+      dxBlockCookie(event.data.data);
       break;
     case "sync":
-      syncConsents(event.data.data.consentCookieId, event.data.data.consents);
+      dxSyncConsents(event.data.data.consentCookieId, event.data.data.consents);
       break;
     case "hideIframe":
-      toggleIframe();
+      dxToggleIframe();
       break;
     case "resizeIframe":
-      resizeIframe(event.data.data.height, event.data.data.width);
+      dxResizeIframe(event.data.data.height, event.data.data.width);
       break;
   }
 }, false);
